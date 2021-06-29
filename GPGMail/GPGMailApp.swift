306e6c062d6886cb1d982798961df0f7ee474ba8
Ollie Hayman <discorev@olliespage.net>
2021-06-29 19:36:32 +0100

    Initial Commit

---
+++ GPGMail/GPGMailApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPGMailApp.swift
+//  GPGMail
+//
+//  Created by Oliver Hayman on 29/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct GPGMailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
